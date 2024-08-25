fcfef7e2d0705a052d9e60316aa90e7306b889c6
toweringcloud <toweringcloud@gmail.com>
2024-08-26 00:47:50 +0900

    go to home after login

--- app/log-in/actions.ts
+++ app/log-in/actions.ts
@@ -48,7 +48,7 @@ export async function logIn(prevState: any, formData: FormData) {
 		const session = await getSession();
 		session.id = user!.id;
 		await session.save();
-		redirect("/profile");
+		redirect("/");
 	} else {
 		return {
 			fieldErrors: {
